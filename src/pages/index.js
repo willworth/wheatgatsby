3566c5df6ac01aff7031a1bf019e57fc266b9949
Will Worth <will.worth@gmail.com>
2018-03-26 09:47:03 +0200

    first

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to the wheat plain Gatsby site.</p>
+    <p>This is the project for the level up tuts gatsby tutorial</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
